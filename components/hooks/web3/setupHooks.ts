1cf61e6108aa2c31913a8883f331ba1ae10bd4f8
saullfeng <2544458760@qq.com>
2022-09-22 16:02:57 +0800

    sethooks update

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -1,19 +1,25 @@
 import { Web3Dependencies } from "@_types/hooks";
 import { hookFactory as createAccountHook, UseAccountHook } from "./useAccount";
 import { hookFactory as createNetworkHook, UseNetworkHook } from "./UseNetwork";
-import { hookFactory as createListedNftsHook, UseListedNftsHook } from "./useListedNfts";
-import { hookFactory as createOwnedNftsHook, UseOwnedNftsHook } from "./useOwnedNfts";
+import {
+  hookFactory as createListedNftsHook,
+  UseListedNftsHook,
+} from "./useListedNfts";
+import {
+  hookFactory as createOwnedNftsHook,
+  UseOwnedNftsHook,
+} from "./useOwnedNfts";
 
 export type Web3Hooks = {
   useAccount: UseAccountHook;
   useNetwork: UseNetworkHook;
   useListedNfts: UseListedNftsHook;
   useOwnedNfts: UseOwnedNftsHook;
-}
+};
 
 export type SetupHooks = {
-  (d: Web3Dependencies): Web3Hooks
-}
+  (d: Web3Dependencies): Web3Hooks;
+};
 
 export const setupHooks: SetupHooks = (deps) => {
   return {
@@ -21,5 +27,5 @@ export const setupHooks: SetupHooks = (deps) => {
     useNetwork: createNetworkHook(deps),
     useListedNfts: createListedNftsHook(deps),
     useOwnedNfts: createOwnedNftsHook(deps),
-  }
-}
+  };
+};
